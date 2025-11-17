1612592fb887b81a26debb855fd7ee8f0a27cd73
Andres <56609693+amrosner@users.noreply.github.com>
2025-11-17 23:28:29 +0000

    added index to redirect to login

--- pages/base.php
+++ pages/base.php
@@ -11,7 +11,7 @@ $content   = $content ?? "";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo htmlspecialchars($pageTitle); ?></title>
 
-    <link rel="stylesheet" href="static/css/style.css">
+    <link rel="stylesheet" href="/static/css/style.css">
 </head>
 
 <body>
